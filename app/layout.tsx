3673caaaa737ba523f795c5b7af71620df65aaaa
Bacale-Kevin <bacale86@gmail.com>
2023-05-04 17:49:35 +0100

    feat: added navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import './globals.css'
-import { Nunito } from 'next/font/google'
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Easy Booking App',
-  description: 'The best and affodable way of booking events and venues',
-}
+  title: "Easy Booking App",
+  description: "The best and affodable way of booking events and venues",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
