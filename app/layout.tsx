a7ec26bc2f0aa2353e91c0fb2a328ff666358143
Bacale-Kevin <bacale86@gmail.com>
2023-05-06 14:18:40 +0100

    feat: implemented login functionlity using next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import { Nunito } from "next/font/google";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -13,14 +15,17 @@ export const metadata = {
   description: "The best and affodable way of booking events and venues",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
