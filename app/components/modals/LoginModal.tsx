ea95949a47f64e51b38a3ab86091d66462d18260
Bacale-Kevin <bacale86@gmail.com>
2023-05-07 23:15:29 +0100

    feat: added social login google and github

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -44,7 +44,7 @@ const LoginModal = () => {
         loginModal.onClose();
       }
       if (callback?.error) {
-        toast.error(callback.error );
+        toast.error(callback.error);
       }
     });
   };
@@ -68,8 +68,8 @@ const LoginModal = () => {
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
-      <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}} />
-      <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => {}} />
+      <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => signIn('google')} />
+      <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => signIn("github")} />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row items-center justify-center gap-2 ">
           <div>Already have an account?</div>
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,6 +11,7 @@ import Headeing from "../Headeing";
 import Input from "../inputs/Input";
 import { toast } from "react-hot-toast";
 import Button from "../Button";
+import { signIn } from 'next-auth/react'
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -59,8 +60,8 @@ const RegisterModal = () => {
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
-      <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}} />
-      <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => {}} />
+      <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => signIn('google')} />
+      <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => signIn('github')} />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row items-center justify-center gap-2 ">
           <div>Already have an account?</div>
@@ -70,7 +71,7 @@ const RegisterModal = () => {
         </div>
       </div>
     </div>
-  );
+  ); 
 
   return (
     <Modal
