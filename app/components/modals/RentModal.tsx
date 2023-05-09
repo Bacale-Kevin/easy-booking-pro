7940e786b39c75aacd15dd597a4fadd9bc57c332
Bacale-Kevin <bacale86@gmail.com>
2023-05-09 16:25:28 +0100

    feat: add description step 5

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
+import Input from "../inputs/Input";
 
 enum STEPS {
   CATEGORY = 0,
@@ -25,6 +26,7 @@ const RentModal = () => {
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY);
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
@@ -150,7 +152,18 @@ const RentModal = () => {
     bodyContent = (
       <div className="flex flex-col gap-8">
         <Heading title="Add a photo of your place" subtitle="Show guests what your place looks like!" />
-        <ImageUpload onChange={(value) => setCustomValue('imageSrc', value)} value={imageSrc} />
+        <ImageUpload onChange={(value) => setCustomValue("imageSrc", value)} value={imageSrc} />
+      </div>
+    );
+  }
+
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading title="How would you describe your place?" subtitle="Short and sweet works best!" />
+        <Input id="title" label="Title" disabled={isLoading} register={register} errors={errors} required />
+        <hr />
+        <Input id="description" label="Description" disabled={isLoading} register={register} errors={errors} required />
       </div>
     );
   }
