b4c5fc92271b4d8d6ce8625b3d84c21a54ebb49d
Bacale-Kevin <bacale86@gmail.com>
2023-05-11 15:50:12 +0100

    fix: fix build errors

--- app/api/reservations/[reservationId]/route.ts
+++ app/api/reservations/[reservationId]/route.ts
@@ -1,27 +1,26 @@
 import { NextResponse } from "next/server";
-import prisma from "@/app/libs/prismadb";
+
 import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
 
 interface IParams {
   reservationId?: string;
 }
 
-export async function DELETE(request: IParams, { params }: { params: IParams }) {
-  const { reservationId } = params;
+export async function DELETE(request: Request, { params }: { params: IParams }) {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) {
     return NextResponse.error();
   }
 
+  const { reservationId } = params;
+
   if (!reservationId || typeof reservationId !== "string") {
     throw new Error("Invalid ID");
   }
 
   const reservation = await prisma.reservation.deleteMany({
-    /** Only the creator of the listing or the creator of the reservation
-     *  should be able to cancel a reservation
-     * */
     where: {
       id: reservationId,
       OR: [{ userId: currentUser.id }, { listing: { userId: currentUser.id } }],
